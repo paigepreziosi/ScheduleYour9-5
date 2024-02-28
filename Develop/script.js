13caf8f96113e5dbb1f1abbde89bd679cc329ba6
Paige Preziosi <paigepreziosi@Paiges-Laptop.lan>
2024-02-28 17:42:09 -0500

    commit loop over timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -11,9 +11,32 @@ $(function () {
   
       localStorage.setItem(time, text);
     })
+  })
+  
+  function timeTracker() {
+    var currentTime = dayjs().hour();
 
     
-  })
+    $(".time-block").each(function () {
+        var rowTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (rowTime < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (rowTime === currentTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
